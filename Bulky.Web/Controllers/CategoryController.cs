6faae1ebce7fd03707c11940b95d329539415a36
jerun276 <jerunkingston@gmail.com>
2024-04-03 12:07:28 +0530

    Vid:20/21 custom validations validation summary

--- Bulky.Web/Controllers/CategoryController.cs
+++ Bulky.Web/Controllers/CategoryController.cs
@@ -22,8 +22,12 @@ namespace Bulky.Web.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Create(Category obj)
+        public IActionResult Create(Category obj) 
         {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            //}
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
@@ -31,7 +35,7 @@ namespace Bulky.Web.Controllers
                 return RedirectToAction("Index");
             }
             return View();
-            
+
         }
     }
 }
